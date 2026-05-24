b459801b5d5c13ed3f5bdfde0dfac4a8530502e1
Banana098905 <kuifunei@gmail.com>
2026-05-24 23:03:04 +0900

    Add files via upload

---
+++ AI.js
@@ -0,0 +1,4 @@
+// ここにCloudflare WorkerのURLを貼り付けてください
+// 例: const WORKER_URL = 'https://my-ai.yourname.workers.dev';
+
+const WORKER_URL = 'https://dash.cloudflare.com/fc3e36ea5df1f876b038bea2805d7484/workers/services/view/portfolio/production/settings';
