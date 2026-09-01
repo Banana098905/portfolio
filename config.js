f300a7306c5c303383722b5766f9f95b9e8ad509
Banana098905 <kuifunei@gmail.com>
2026-09-01 15:03:52 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Cloudflare WorkerをデプロイしたあとにここへURLを貼り付けてください
+// 例: const WORKER_URL = "https://my-ai.your-subdomain.workers.dev";
+const WORKER_URL = "https://raspy-glade-75bf.kuifunei.workers.dev";
